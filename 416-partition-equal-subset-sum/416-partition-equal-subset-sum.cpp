58ba222c5a035bc381c3f490274969b6672ccd3b
JATIN DAYAL <94393895+procojd@users.noreply.github.com>
2022-07-27 22:46:37 +0530

    Time: 366 ms (40.10%), Space: 75.4 MB (27.02%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+bool solveTab(vector<int>& nums, int n, int sum)
+{
+    vector<vector<int>> dp(n+1,vector<int>(sum+1,0)); 
+    for(int i=0;i<=n;i++)
+        dp[i][0]=1;
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sum;j++)
+        {
+            if(nums[i-1]>j)
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j] || dp[i-1][j-nums[i-1]];
+            }
+        }
+    }
+    return dp[n][sum];
+}
+
+bool canPartition(vector<int>& nums) {
+    int n=nums.size();
+    int sum=0;
+    for(auto x: nums)
+    {
+        sum+=x;
+    }
+    if(sum%2)
+        return false;
+    int target=sum/2;
+    // vector<vector<int>> dp(n,vector<int>(target+1,-1));
+    return solveTab(nums, n-1,target);
+}
+};
