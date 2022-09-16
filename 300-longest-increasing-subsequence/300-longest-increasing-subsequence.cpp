d589a385a9e043aa81daa413fffc447fbe4515db
JATIN DAYAL <94393895+procojd@users.noreply.github.com>
2022-09-16 12:25:52 +0530

    Time: 379 ms (49.65%), Space: 10.6 MB (44.33%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        
-        vector<int> sub;
-        
-        int n = nums.size();
-        
-        for(int i = 0; i < n; i++) {
-            auto it = lower_bound(sub.begin(), sub.end(), nums[i]);
-            if(it != sub.end()) {
-                *it = nums[i];
-            } else {
-                sub.push_back(nums[i]);
-            }
+     int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n, 1);
+        int ans=1;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++) if(nums[j]<nums[i])
+                dp[i]=max(dp[i], dp[j]+1);
+            ans=max(ans, dp[i]);
         }
-        return sub.size();
+        return ans;
     }
 };
